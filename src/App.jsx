1d294a9f8600d27033a011133f7c4dd8889f85be
AntonyC12 <coelloantony1212@gmail.com>
2026-04-18 20:00:06 -0500

    first commit: Initial project

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  const [numberOne, setNumberOne] = useState(0);
+  const [numberTwo, setNumberTwo] = useState(0);
+  
+  function sum() {
+    const sum = numberOne + numberTwo;
+    window.alert('The sum is: ' + sum);
+  }
+
+  return (
+    <>
+        <form>
+          <h1>Calculator for Two Numbers</h1>
+          <br/>
+          <br/>
+          <h2>Number 1</h2>
+          <input type="number" onChange={(e) => setNumberOne(Number(e.target.value))} />
+          <br/>
+          <br/>
+          <h2>Number 2</h2>
+          <input type="number" onChange={(e) => setNumberTwo(Number(e.target.value))} />
+          <br/>
+          <br/>
+          <button type="submit" onClick={sum}>Sum</button>
+          <br/>
+        </form>
+    </>
+  )
+}
+
+export default App

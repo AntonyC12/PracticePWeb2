2b1bd8760228120154ce76d4d06fce402f783a13
AntonyC12 <coelloantony1212@gmail.com>
2026-04-20 16:36:10 -0500

    feat: implement basic two-number calculator component in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import './App.css'
 function App() {
   const [numberOne, setNumberOne] = useState(0);
   const [numberTwo, setNumberTwo] = useState(0);
-  
+
   function sum() {
     const sum = numberOne + numberTwo;
     window.alert('The sum is: ' + sum);
@@ -15,21 +15,21 @@ function App() {
 
   return (
     <>
-        <form>
-          <h1>Calculator for Two Numbers</h1>
-          <br/>
-          <br/>
-          <h2>Number 1</h2>
-          <input type="number" onChange={(e) => setNumberOne(Number(e.target.value))} />
-          <br/>
-          <br/>
-          <h2>Number 2</h2>
-          <input type="number" onChange={(e) => setNumberTwo(Number(e.target.value))} />
-          <br/>
-          <br/>
-          <button type="submit" onClick={sum}>Sum</button>
-          <br/>
-        </form>
+      <form>
+        <h1>Calculator for Two Number</h1>
+        <br />
+        <br />
+        <h2>Number 1</h2>
+        <input type="number" onChange={(e) => setNumberOne(Number(e.target.value))} />
+        <br />
+        <br />
+        <h2>Number 2</h2>
+        <input type="number" onChange={(e) => setNumberTwo(Number(e.target.value))} />
+        <br />
+        <br />
+        <button type="submit" onClick={sum}>Sum</button>
+        <br />
+      </form>
     </>
   )
 }
